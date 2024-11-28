b32d3b7df424f1d031dfd3e7fb5f57f9c25eee51
Alessandro-Frisone <alefri2004@gmail.com>
2024-11-28 14:42:13 +0100

    feat: fianl discount price

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1-Chiediamo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+const km = prompt ("Quanti KM vuole percorrere?")
+const age = prompt ("Quanti anni ha il passeggero?")
+// 2-Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const priceForKm = 0.21;
+let price = km * priceForKm;
+// 3-Va applicato uno sconto del 20% per i minorenni
+if (age <= 18) {
+  price -= price * 0.20; 
+}else
+// 4-Va applicato uno sconto del 40% per gli over 65.
+ if (age >= 65){
+    price -= price * 0.40;
+}
+// Output del prezzo finale con massimo due decimali
+alert(`Il prezzo del biglietto è: €${price.toFixed(2)}`);
